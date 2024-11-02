c03cfe1a5099ff67336d7fef92e6abba4f08e700
ManuelRCreative <160515463+ManuelRCreative@users.noreply.github.com>
2024-11-01 21:35:29 -0400

    First lesson and configuration

---
+++ 01-no-frills-react/src/App.js
@@ -0,0 +1,41 @@
+const Pizza = function (props) {
+  return React.createElement("div", {}, [
+    React.createElement(
+      "h1",
+      {},
+      props.name ? props.name : "oops... data is empty"
+    ),
+    React.createElement("p", {}, props.description),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Padre Gino's"),
+    React.createElement(Pizza, {
+      name: "The pepperoni pizza",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lacinia bibendum nulla sed consectetur. Nullam quis risus eget urna mollis ornare vel eu leo.",
+    }),
+    React.createElement(Pizza, {
+      name: "The mozzarella pizza",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lacinia bibendum nulla sed consectetur. Nullam quis risus eget urna mollis ornare vel eu leo.",
+    }),
+    React.createElement(Pizza, {
+      name: "The cheese pizza",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lacinia bibendum nulla sed consectetur. Nullam quis risus eget urna mollis ornare vel eu leo.",
+    }),
+    React.createElement(Pizza, {
+      name: "The pineapple pizza",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lacinia bibendum nulla sed consectetur. Nullam quis risus eget urna mollis ornare vel eu leo.",
+    }),
+    React.createElement(Pizza, {}),
+  ]);
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
